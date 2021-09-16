53fa57eae943f6990cb05cc9ad2d7465cf9d4143
alexkerius <86520137+alexkerius@users.noreply.github.com>
2021-09-16 23:31:09 +0300

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,26 @@
+public class Variable {
+    public static void main(String[] args){
+        byte num=8;
+        short num1=128;
+        int num2=1600;
+        long num3=900;
+        float num4=90.5242f;
+        double num5=160.127827872d;
+        boolean boo=true;
+        char symbol='a';
+        String str="basis";
+
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(boo);
+        System.out.println(symbol);
+        System.out.println(str);
+        
+
+    }
+    
+}
